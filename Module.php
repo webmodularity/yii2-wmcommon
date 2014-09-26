bc67b868a900c4a8a4c5695c05caf4a5cc51efa6
Rory <rory@webmodularity.com>
2014-09-26 11:24:20 -0700

    Initial commit

---
+++ Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace wmcommon;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'wmcommon\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+}
