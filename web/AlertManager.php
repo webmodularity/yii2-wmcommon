d7fa30112e32a98084ec57c4f2650741feb92029
Rory Hellyer <rory@webmodularity.com>
2015-06-30 07:23:44 +0000

    dev

--- web/AlertManager.php
+++ web/AlertManager.php
@@ -24,38 +24,4 @@ class AlertManager extends \yii\base\Component
         $alerts = Yii::$app->session->getFlash($flashId, []);
         return implode($glue, $alerts);
     }
-
-    /** Depreciated - use addAlert instead */
-    /*
-    public function Dadd($style, $message, $heading = null, $options = [], $flashId = self::DEFAULT_FLASH_ID) {
-        if ($style == 'error') {
-            $style = 'danger';
-        }
-        $class = $this->alertClass;
-        $settings = compact('class', 'style', 'message', 'heading');
-        $config = ArrayHelper::merge($settings, $options);
-        $alertObject = Yii::createObject($config);
-        if (!is_null($alertObject)) {
-            $alertHtml = $alertObject->run();
-            if ($alertHtml) {
-                $flashArray = Yii::$app->session->hasFlash($flashId)
-                    ? Yii::$app->session->getFlash($flashId)
-                    : $this->_alertTypes;
-                $flashArray[$style][] = $alertHtml;
-                Yii::$app->session->setFlash($flashId, $flashArray);
-            }
-        }
-    }
-
-    public function render($flashId = self::DEFAULT_FLASH_ID) {
-        $alertHtml = '';
-        $alerts = Yii::$app->session->getFlash($flashId, []);
-        foreach ($alerts as $alertStyle => $alertList) {
-            foreach ($alertList as $alert) {
-                $alertHtml .= $alert;
-            }
-        }
-        return $alertHtml;
-    }
-    */
 }
